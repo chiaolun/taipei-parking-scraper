a8fb33f681237f06e32dc3737bbb43eb3ae919a8
Chiao Cheng <chiao@alum.mit.edu>
2021-11-29 20:18:38 -0800

    subfolders for data

--- download.sh
+++ download.sh
@@ -12,12 +12,16 @@ if [[ $1 == "meta" ]]; then
     META_TIME=$(date --date="$META_TIME" --iso-8601=seconds)
     META_MD5=$(echo -nE "$META_PAYLOAD" | md5sum | awk '{print $1}')
 
-    echo -nE "$META_PAYLOAD" | gzip > "data/${META_TIME}_meta_${META_MD5}.gz"
+    TARGET="data/${META_TIME:0:10}/${META_TIME}_meta_${META_MD5}.gz"
+    mkdir -p $(dirname $TARGET)
+    echo -nE "$META_PAYLOAD" | gzip > $TARGET
 elif [[ $1 == "info" ]]; then
     INFO_PAYLOAD=$(curl -sS $URL_INFO)
     INFO_TIME=$(echo -nE "$INFO_PAYLOAD" | jq -r .data.UPDATETIME)
     INFO_TIME=$(date --date="$INFO_TIME" --iso-8601=seconds)
     INFO_MD5=$(echo -nE "$INFO_PAYLOAD" | md5sum | awk '{print $1}')
 
-    echo -nE "$INFO_PAYLOAD" | gzip > "data/${INFO_TIME}_info_${INFO_MD5}.gz"
+    TARGET="data/${INFO_TIME:0:10}/${INFO_TIME}_info_${INFO_MD5}.gz"
+    mkdir -p $(dirname $TARGET)
+    echo -nE "$INFO_PAYLOAD" | gzip > $TARGET
 fi
